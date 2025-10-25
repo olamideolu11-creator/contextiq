971c061f2699f0084a82bfda88ce660cefe3e5fb
olamideolu11-creator <olamideolu11@gmail.com>
2025-10-25 15:42:49 -0400

    Delete commands.js

--- commands.js
+++
@@ -1,8 +0,0 @@
-function showTaskPane(event) {
-  Office.addin.showAsTaskpane()
-    .then(() => event.completed())
-    .catch(error => {
-      console.error("Failed to show taskpane:", error);
-      event.completed();
-    });
-}
